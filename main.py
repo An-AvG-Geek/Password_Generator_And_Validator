0a054599076548fd0264eec7c4c077e99fb6af56
Felix Jobi <107713717+An-AvG-Geek@users.noreply.github.com>
2024-01-08 23:54:07 +0530

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ def main():
     st.sidebar.title("Password Toolkit")
    
 
-    choice = st.sidebar.selectbox("Password Toolkit",["Password Generator", "Password Validator"])
+    choice = st.sidebar.option_menu(menu_title="Password Toolkit",options=["Password Generator", "Password Validator"])
     if choice == "Password Generator":
         st.title("Password Generator")
         length = int(get_length())
