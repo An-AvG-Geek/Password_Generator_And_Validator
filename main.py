36fc49a309bf31884f40a953cd34cc002631b1d9
Felix Jobi <107713717+An-AvG-Geek@users.noreply.github.com>
2024-01-08 15:55:23 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import streamlit as st
 
 def main():
     st.title("Password Generator and Validator")
+    st.write("this is just a trial writing...")
 
     choice = st.selectbox("MENU", ["Password Generator", "Password Validator"])
     if choice == "Password Generator":
