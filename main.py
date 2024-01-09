0bbfdf39d1adf37fc94fd53ceb2cf07dfdb62e68
Felix Jobi <107713717+An-AvG-Geek@users.noreply.github.com>
2024-01-09 18:40:09 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
 import random
 import streamlit as st
-
+from streamlit_option_menu import option_menu
 
 
 def main():
-    st.set_page_config(page_title="Password Toolkit",page_icon="🔐")
-    st.sidebar.title("Password Toolkit")
-   
-
-    choice = st.sidebar.selectbox("Password Toolkit",["Password Generator", "Password Validator"])
+    with st.sidebar:
+        choice = option_menu(
+            menu_title="Password Toolkit",
+            options=["Password Generator", "Password Validator"],
+            menu_icon="tools",
+            icons=["key", "shield-lock"],
+        )
     if choice == "Password Generator":
         st.title("Password Generator")
         length = int(get_length())
@@ -34,63 +36,52 @@ def generate_password(len):
 
 
 def validator():
-   
     password = st.text_input("Enter your password ", "password....")
     if st.button("Submit"):
         parameters = check(password)
-        count=0
+        count = 0
 
-        if len(password)>=6:
+        if len(password) >= 6:
             st.success("Length is >6")
-            count+=1
-        else :
+            count += 1
+        else:
             st.error("The length is less than requirement")
 
-
-        if parameters[0]>=2:
+        if parameters[0] >= 2:
             st.success("Uppercase present")
-            count+=1
+            count += 1
         else:
             st.error("Uppercase characters not satisfied  ")
 
-        if parameters[1]>=2:
+        if parameters[1] >= 2:
             st.success("Lowercase present")
-            count+=1
+            count += 1
         else:
             st.error("Lowercase characters not satisfied")
 
-        if parameters[2]>=1:
+        if parameters[2] >= 1:
             st.success("Digits present ")
-            count+=1
+            count += 1
         else:
             st.error("Numeric characters not satisfied ")
 
-        if parameters[3]>=1:
+        if parameters[3] >= 1:
             st.success("Special characters present ")
-            count+=1
+            count += 1
         else:
             st.error("Special characters not satisfied")
 
-        if parameters[4]>0:
+        if parameters[4] > 0:
             st.error("Space is present")
         else:
             st.success("No space is found ")
-            count+=1
+            count += 1
 
-        percent=(count/6)*100
-        st.progress(int(percent),"STRENGTH")
+        percent = (count / 6) * 100
+        st.progress(int(percent), "STRENGTH")
 
-        if percent==100:
+        if percent == 100:
             st.balloons()
-
-
-
-        
-       
-
-        
-
-    
 
 
 def check(password):
@@ -98,7 +89,7 @@ def check(password):
     lower = 0
     special = 0
     digit = 0
-    space=0
+    space = 0
 
     for i in password:
         if i.isupper():
@@ -110,13 +101,10 @@ def check(password):
         if i.isalnum() == False:
             special += 1
         if i.isspace():
-            space+=1
-            special-=1
+            space += 1
+            special -= 1
 
-    return [upper,lower,digit,special,space]
-        
-
-    
+    return [upper, lower, digit, special, space]
 
 
 if __name__ == "__main__":
